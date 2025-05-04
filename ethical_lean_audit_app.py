c0391216195ce6c45a9017eae8b5f25d69b17d53
LEAN-arch <jbautistamsc@gmail.com>
2025-05-03 20:20:45 -0700

    Update ethical_lean_audit_app.py

--- ethical_lean_audit_app.py
+++ ethical_lean_audit_app.py
@@ -1039,8 +1039,8 @@ if not st.session_state.show_intro:
                         worksheet_summary.write(row, 0, "Colabora con LEAN 2.0 Institute" if st.session_state.language == "Español" else "Partner with LEAN 2.0 Institute", bold)
                         row += 1
                         invitation = (
-                            "¡Colabora con LEAN 2.0 Institute para implementar mejoras en tu lugar de trabajo! Contáctanos para una consulta estratégica." if st.session_state.language == "Español" else
-                            "Partner with LEAN 2.0 Institute to implement workplace improvements! Contact us for a strategic consultation."
+                            "¡Colaboremos para implementar mejoras en tu lugar de trabajo! Contáctanos para una consulta estratégica." if st.session_state.language == "Español" else
+                            "Let's partner withto implement workplace improvements! Contact us for a strategic consultation."
                         )
                         worksheet_summary.write(row, 0, invitation, wrap_format)
                         row += 1
